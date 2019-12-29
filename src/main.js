68e579a3db89472b3029948d35562f21581fd85e
Sunil More <sunilmore690@gmail.com>
2019-12-29 16:19:03 +0530

    default lang set to en

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 // import store from './store';
 import ElementUI from 'element-ui';
+import locale from "element-ui/lib/locale/lang/en";
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false;
 
-Vue.use(ElementUI)
+Vue.use(ElementUI,{locale})
 new Vue({
   // store,
   render: h => h(App)
